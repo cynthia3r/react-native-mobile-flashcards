1cdec3e4b38fdc3381cf0eed95d0d408c09e070d
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-26 02:48:02 +1000

    feat: add navigation for QuizResult

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,10 +4,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FlashcardsStatusBar from '../components/FlashcardsStatusBar';
 import DeckList from '../components/DeckList';
-import Deck from '../components/Deck';
-import NewDeck from '../components/NewDeck';
-import NewQuestion from '../components/NewQuestion';
+import DeckDetail from '../components/DeckDetail';
+import AddDeck from '../components/AddDeck';
+import AddCard from '../components/AddCard';
 import Quiz from '../components/Quiz';
+import QuizResult from '../components/QuizResult';
 import { purple } from '../utils/colors';
 
 const Tab = createBottomTabNavigator();
@@ -16,8 +17,14 @@ const Stack = createStackNavigator();
 function Dashboard() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name='Deck List' component={DeckList} />
-            <Tab.Screen name='Add Deck' component={NewDeck} />
+            <Tab.Screen 
+                name='Deck List'
+                component={DeckList}
+            />
+            <Tab.Screen
+                name='Add Deck'
+                component={AddDeck}
+            />
         </Tab.Navigator>
     );
 }
@@ -31,20 +38,24 @@ export default function AppNavigator() {
             />
             <Stack.Navigator>
                 <Stack.Screen
-                    name='Dashboard'
+                    name='MobileFlashcards'
                     component={Dashboard}
                 />
                 <Stack.Screen
-                    name='Deck'
-                    component={Deck}
+                    name='DeckDetail'
+                    component={DeckDetail}
                 />
                 <Stack.Screen
                     name='AddCard'
-                    component={NewQuestion}
+                    component={AddCard}
                 />
                 <Stack.Screen
                     name='Quiz'
                     component={Quiz}
+                />
+                 <Stack.Screen
+                    name='QuizResult'
+                    component={QuizResult}
                 />
             </Stack.Navigator>
         </NavigationContainer>
