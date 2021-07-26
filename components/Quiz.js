ea8a2f85cd501b281eef65f4ca0a8c9dbba3d389
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-27 03:41:32 +1000

    feat: implement quiz functionality

---
+++ components/Quiz.js
@@ -0,0 +1,172 @@
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { connect } from 'react-redux';
+import { green, red, white, orange} from '../utils/colors';
+
+
+function Quiz(props) {
+    const navigation = useNavigation();
+    const deckId = props.route.params.title;
+    const cards = props.decks[deckId].cards;
+
+    const [index, setIndex] = React.useState(0);
+    const [numCorrectAnswers, setNumCorrectAnswers] = React.useState(0);
+    const [numIncorrectAnswers, setNumIncorrectAnswers] = React.useState(0);
+    const [showAnswer, setStatus] = React.useState('false');
+    const [quizCompletionStatus, setQuizCompletionStatus] = React.useState('false');
+
+    useEffect(() => {
+        if (quizCompletionStatus === 'true') {
+            console.log('final: ', numCorrectAnswers);
+            resetQuiz();
+        }
+    }, [quizCompletionStatus]);
+
+    const resetQuiz = () => {
+        setIndex(0);
+        setNumCorrectAnswers(0);
+        setNumIncorrectAnswers(0);
+        setStatus('false');
+        setQuizCompletionStatus('false');
+
+        navigation.navigate('QuizResult', {
+            id: deckId,
+            numIncorrectAnswers,
+            numCorrectAnswers,
+        });
+    };
+
+    function nextQuestion() {
+        setStatus('false');
+        if(index + 1 !== cards.length) {
+            setIndex(index + 1);
+        } else {
+            setIndex(index);
+            setQuizCompletionStatus('true');
+        }
+    }
+
+    if(cards.length === 0) {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.title}>
+                        There are no cards in the deck
+                    </Text>
+                </View>
+                <View>
+                    <TouchableOpacity
+                        style={[styles.btn, {backgroundColor: green}]}
+                        onPress={() => navigation.navigate(
+                            'DeckDetail',
+                            {title: deckId }
+                        )}
+                    >
+                        <Text style={styles.btnTitle}>Back to Deck</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    } else {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.title}>
+                        Question {index + 1}/{cards.length}
+                    </Text>
+                </View>
+                <View>
+                    <Text style={styles.question}>
+                        {cards[index].question}
+                    </Text>
+                    {showAnswer === 'false' ? (
+                        <TouchableOpacity
+                            onPress={() => setStatus('true')}
+                        >
+                            <Text style={styles.showAnswerTitle}>Show Answer</Text>
+                        </TouchableOpacity> ) : (
+                        <View>
+                            <Text style={styles.answer}>
+                                {cards[index].answer}
+                            </Text>
+                        </View>   
+                        )}
+                </View>
+                <View>
+                    <TouchableOpacity
+                        style={[styles.btn, {backgroundColor: green}]}
+                        onPress={() => {
+                            nextQuestion();
+                            setNumCorrectAnswers(numCorrectAnswers + 1);
+                        }}
+                    >
+                        <Text style={styles.btnTitle}>Correct</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        // key={deck.title}
+                        style={[styles.btn, {backgroundColor: red}]}
+                        onPress={() => {
+                            nextQuestion();
+                            setNumIncorrectAnswers(numIncorrectAnswers + 1);
+                        }}
+                    >
+                        <Text style={styles.btnTitle}>Incorrect</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+  
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 20,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    showAnswerTitle: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: orange,
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+        color: green,
+    },
+    question: {
+        textAlign: 'center',
+        fontSize: 20,
+    },
+    answer: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: orange,
+    },
+    btn: {
+        marginBottom: 20,
+        width: 150,
+        height: 30,
+        borderWidth: 1,
+    },
+    btnTitle: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: white,
+    }
+});
+
+function mapStateToProps(state) {
+    return {
+        decks: state,
+    };
+}
+
+
+export default connect(mapStateToProps)(Quiz);
---
+++ components/QuizResult.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import Deck from './Deck';
+import { green, white, orange } from '../utils/colors';
+
+
+function QuizResult(props) {
+    const navigation = useNavigation();
+
+    console.log("props:", props);
+
+    const decks = props.decks;
+    const deckId = props.route.params.id;
+    const numIncorrectAnswer = props.route.params.numIncorrectAnswers;
+    const numCorrectAnswer = props.route.params.numCorrectAnswers;
+    const totalQuestions = numIncorrectAnswer + numCorrectAnswer;
+
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.title}>
+                    Correct Answers:
+                </Text>
+                <Text style={styles.result}>
+                    {numCorrectAnswer} out of {totalQuestions}
+                </Text>
+            </View>
+            <View>
+                <Text style={styles.title}>
+                    Percentage Correct:
+                </Text>
+                <Text style={styles.result}>
+                    {Math.round((numCorrectAnswer * 100) / (totalQuestions))} %
+                </Text>
+            </View>
+            <View>
+                <TouchableOpacity
+                    style={styles.btn}
+                    onPress={() => navigation.navigate(
+                        'DeckDetail',
+                        {title: deckId }
+                    )}
+                >
+                    <Text style={styles.btnTitle}>Back to Deck</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.btn}
+                    onPress={() => navigation.navigate(
+                        'Quiz',
+                        {title: deckId }
+                    )}
+                >
+                    <Text style={styles.btnTitle}>Restart Quiz</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    ); 
+}
+  
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 20,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 30,
+        color: green,
+    },
+    btn: {
+        backgroundColor: green,
+        marginBottom: 20,
+        width: 150,
+        height: 30,
+        borderWidth: 1,
+    },
+    btnTitle: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: white,
+    },
+    result: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: orange,
+    },
+});
+
+function mapStateToProps(state) {
+    const decks = state;
+
+    return {
+        decks,
+    };
+}
+
+export default connect(mapStateToProps)(QuizResult);
