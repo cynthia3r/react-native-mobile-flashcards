7efba25897278b75e7fb7c544a2047f3886de9b0
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-26 18:40:30 +1000

    style: added ionicons for tab navigator components

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import FlashcardsStatusBar from '../components/FlashcardsStatusBar';
 import DeckList from '../components/DeckList';
 import DeckDetail from '../components/DeckDetail';
@@ -9,21 +11,49 @@ import AddDeck from '../components/AddDeck';
 import AddCard from '../components/AddCard';
 import Quiz from '../components/Quiz';
 import QuizResult from '../components/QuizResult';
-import { purple } from '../utils/colors';
+import { green, gray } from '../utils/colors';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 function Dashboard() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen 
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon: ({ focused, size, color }) => {
+                    if (route.name === 'Deck List') {
+                        return (
+                            <Ionicons 
+                            name={focused ? 'ios-bookmarks' : 'md-bookmarks'}
+                            size={size}
+                            color={color}
+                            />
+                        );
+                    } else if (route.name === 'Add Deck') {
+                        return (
+                            <Ionicons 
+                            name={focused ? 'add-circle' : 'add-circle-outline'}
+                            size={size}
+                            color={color}
+                            />
+                        );
+                    }
+                }
+            })}
+            tabBarOptions={{
+                activeTintColor: green,
+                inactiveTintColor: gray,
+            }}
+        >
+            <Tab.Screen
                 name='Deck List'
                 component={DeckList}
+                options={{ title: 'Deck List', headerShown: true }}
             />
             <Tab.Screen
                 name='Add Deck'
                 component={AddDeck}
+                options={{ title: 'Add Deck', headerShown: true }}
             />
         </Tab.Navigator>
     );
@@ -32,33 +62,42 @@ function Dashboard() {
 export default function AppNavigator() {
     return(
         <NavigationContainer>
-            <FlashcardsStatusBar 
-                backgroundColor={purple}
-                barStyle='light-content'
-            />
-            <Stack.Navigator>
-                <Stack.Screen
-                    name='MobileFlashcards'
-                    component={Dashboard}
+            <View style={styles.container}>
+                <FlashcardsStatusBar 
+                    backgroundColor={green}
+                    barStyle='light-content'
                 />
-                <Stack.Screen
-                    name='DeckDetail'
-                    component={DeckDetail}
-                />
-                <Stack.Screen
-                    name='AddCard'
-                    component={AddCard}
-                />
-                <Stack.Screen
-                    name='Quiz'
-                    component={Quiz}
-                />
-                 <Stack.Screen
-                    name='QuizResult'
-                    component={QuizResult}
-                />
-            </Stack.Navigator>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name='Mobile Flashcards'
+                        component={Dashboard}
+                    />
+                    <Stack.Screen
+                        name='DeckDetail'
+                        component={DeckDetail}
+                    />
+                    <Stack.Screen
+                        name='AddCard'
+                        component={AddCard}
+                    />
+                    <Stack.Screen
+                        name='Quiz'
+                        component={Quiz}
+                    />
+                    <Stack.Screen
+                        name='QuizResult'
+                        component={QuizResult}
+                    />
+                </Stack.Navigator>
+            </View>
         </NavigationContainer>
 
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    }
+});
